3a9baba6d8df92bc708775755c27d0e4bf5a7845
Ryan Harley <rharley.dev@gmail.com>
2020-09-08 15:08:50 -0500

    cleaned up and created reducer

---
+++ src/Controllers/Redux/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// createSlice easy way to create reducer with initial state in one const
+const slice = createSlice({
+  name: 'auth',
+  initialState: {
+    admin: false,
+    loggedIn: false,
+  },
+  reducers: {
+    signIn: (state, action) => {
+      const { name, password } = action.payload;
+      state.loggedIn = true;
+      state.admin = true
+    },
+    signOut: state => {
+      state.loggedIn = false;
+      state.admin = false
+    },
+    createUser: (state, action) => {},
+  },
+});
+
+export default slice.reducer;
