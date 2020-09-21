a9bb9c8a2b173ea532c5fa0d3e71c11aefb9fc68
Ryan Harley <rharley.dev@gmail.com>
2020-09-21 15:29:13 -0500

    finishing setting up redux to have state within app

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/userSlice';
+
+// organize reducers within the state
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer,
+});
+
+const store = configureStore({
+  reducer, // same as reducer: reducer
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
