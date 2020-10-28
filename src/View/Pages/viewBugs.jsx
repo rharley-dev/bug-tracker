e716b0bc9ceb0379cb053cf9b8b662608c42b185
Ryan Harley <rharley.dev@gmail.com>
2020-10-28 18:41:20 -0500

    changed color naming and added bug info

--- src/View/Pages/viewBugs.jsx
+++ src/View/Pages/viewBugs.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import BugView from '../BugView/bugView';
 import { getBugs } from '../../Controllers/Redux/bugSlice';
 import BugCard from '../BugCard/bugCard';
-import './viewBugs.css';
 
 export default function Bugs() {
+  const [displayBug, setDisplayBug] = useState({
+    name: '',
+    isDisplayed: false,
+  });
   const dispatch = useDispatch();
   const { bugs } = useSelector(state => state);
 
-  function bugClicked(name) {}
+  function bugClicked(name) {
+    setDisplayBug({
+      name: name,
+      isDisplayed: !displayBug.isDisplayed,
+    });
+  }
 
   useEffect(() => {
     dispatch(getBugs());
@@ -19,6 +28,14 @@ export default function Bugs() {
       {bugs.map((bug, key) => (
         <BugCard key={key} bug={bug} clicked={bugClicked} />
       ))}
+      {/* filter passed in bug with corisponding name */}
+      {displayBug.isDisplayed && (
+        <BugView
+          bug={bugs.filter(bug => {
+            return bug.name == displayBug.name;
+          })}
+        />
+      )}
     </div>
   );
 }
